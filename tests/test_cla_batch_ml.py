192eacb2c8a189c830cd258b506cbf5ff2e2030f
Thang Bui <thang.bui@uber.com>
2020-01-28 13:29:15 -0800

    updated test script

--- tests/test_cla_batch_ml.py
+++ tests/test_cla_batch_ml.py
@@ -65,40 +65,40 @@ def plot_prediction_four(pred_prob, x_train, y_train, X1, X2, zu, title):
         pad_inches=0,
     )
 
+if __name__ == "__main__":
+    x_train, y_train, x_plot, X1, X2 = get_toy_cla_four()
+    x_train = torch.from_numpy(x_train).to(torch.float)
+    y_train_one_hot = torch.from_numpy(y_train)
+    y_train_class = torch.from_numpy(np.argmax(y_train, 1))
+    x_plot = torch.from_numpy(x_plot).to(torch.float)
 
-x_train, y_train, x_plot, X1, X2 = get_toy_cla_four()
-x_train = torch.from_numpy(x_train).to(torch.float)
-y_train_one_hot = torch.from_numpy(y_train)
-y_train_class = torch.from_numpy(np.argmax(y_train, 1))
-x_plot = torch.from_numpy(x_plot).to(torch.float)
+    Din = x_train.shape[1]
+    Dout = 4
+    kern = RBFKernel(Din)
+    lik = MulticlassSoftmax()
+    M = 20
+    model = SparseGP(Din, Dout, M, kern, lik)
+    optimizer = Adam(model.parameters(), lr=0.01)
+    no_epochs = 5000
+    k_train = 10
+    N_total = x_train.shape[0]
+    for e in range(no_epochs):
+        optimizer.zero_grad()
+        losses = model.loss(x_train, y_train_class, no_samples=k_train)
+        # in case when we need to do minibatching
+        scale = x_train.shape[0] / N_total
+        loss = losses[0] + losses[1]
+        loss.backward()
+        optimizer.step()
+        if e % 10 == 0 or e == no_epochs - 1:
+            print("epoch {} / {}, loss {}".format(e, no_epochs, loss.item()))
 
-Din = x_train.shape[1]
-Dout = 4
-kern = RBFKernel(Din)
-lik = MulticlassSoftmax()
-M = 20
-model = SparseGP(Din, Dout, M, kern, lik)
-optimizer = Adam(model.parameters(), lr=0.01)
-no_epochs = 5000
-k_train = 10
-N_total = x_train.shape[0]
-for e in range(no_epochs):
-    optimizer.zero_grad()
-    losses = model.loss(x_train, y_train_class, no_samples=k_train)
-    # in case when we need to do minibatching
-    scale = x_train.shape[0] / N_total
-    loss = losses[0] + losses[1]
-    loss.backward()
-    optimizer.step()
-    if e % 10 == 0 or e == no_epochs - 1:
-        print("epoch {} / {}, loss {}".format(e, no_epochs, loss.item()))
-
-k_test = 100
-with torch.no_grad():
-    pred_logprobs = model.predict(x_plot, pred_y=True, no_samples=k_test)
-    loglik = torch.logsumexp(pred_logprobs, dim=-1) - np.log(k_test)
-    pred_probs = torch.exp(loglik)
-zu = model.xu.detach()
-plot_prediction_four(
-    pred_probs, x_train, y_train_class, X1, X2, zu, "four_batch_ml"
-)
+    k_test = 100
+    with torch.no_grad():
+        pred_logprobs = model.predict(x_plot, pred_y=True, no_samples=k_test)
+        loglik = torch.logsumexp(pred_logprobs, dim=-1) - np.log(k_test)
+        pred_probs = torch.exp(loglik)
+    zu = model.xu.detach()
+    plot_prediction_four(
+        pred_probs, x_train, y_train_class, X1, X2, zu, "four_batch_ml"
+    )
