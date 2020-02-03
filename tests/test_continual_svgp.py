2ed6ceba301dd20f40bb54c19489bee3d66d6334
Sanyam Kapoor <sanyam@uber.com>
2020-02-03 11:39:12 -0800

    Isolate conditional distributions into a function

--- tests/test_continual_svgp.py
+++ tests/test_continual_svgp.py
@@ -160,19 +160,59 @@ class ContinualSVGP(nn.Module):
     # New inducing points
     self.z = nn.Parameter(z_init.detach())
 
-    # Variational parameters for q(u)
+    # Variational parameters for q(u_t | u_{<t})
     out_size = self.z.size(0)
     self.u_mean = nn.Parameter(torch.Tensor(out_size, self.M, 1).normal_(0., .5))
     self.u_tril_vec = nn.Parameter(torch.ones(out_size, (self.M * (self.M + 1)) // 2))
 
-    self.register_buffer('jitter', jitter * torch.eye(self.M))
+    self.jitter = jitter
 
-  def forward(self, x, info=False):
+  def linear_gauss_conditional(self, x, z, u_mean, u_tril, theta):
     '''
-    effectively the marginal p(f|X,θ) of the joint p(f,u|X,θ)
-    approximated by q(f,u|X,θ) = p(f|X,u,θ)q(u|θ)
-    for k samples of θ.
-    
+    Utility function, linear Gaussian systems style,
+    gets the marginal p(f|x,θ) of the joint
+    p(f,u|x,z,θ) = p(f|x,u,θ)p(u|z,θ)
+
+    Marginals are given by:
+
+    mu = K(x,z|θ)Kinv(z,z|θ)u
+    cov = K(x,x|θ) - K(x,z|θ)Kinv(z,z|θ)K(z,x|θ) + K(x,z|θ)Kinv(z,z|θ)(Su)Kinv(z,z|θ)K(z,x|θ)
+
+    The code below only keeps diagonal covariance.
+
+    Arguments:
+      x: data, B x in_size
+      z: inducing points, out_size x M x in_size
+      u_mean: variational dist. mean, out_size x M x 1
+      u_tril: variational dist. scale_tril, out_size x M x M
+      theta: kernel hyperparameters n_hypers x theta_size
+
+    Returns:
+      mu: out means, n_hypers x out_size x B
+      var: out variances, n_hypers x out_size x B
+    '''
+    kfu = self.kernel.compute_kfu(x, z, theta)
+    kuf = kfu.permute(0, 1, 3, 2)
+    kuu = self.kernel.compute_kuu(z, theta)
+    Lkuu = torch.cholesky(kuu + self.jitter * torch.eye(kuu.size(-1), device=kuu.device), upper=False)
+    LKinvu, _ = torch.triangular_solve(u_mean, Lkuu, upper=False)
+    LKinvKuf, _ = torch.triangular_solve(kuf, Lkuu, upper=False)
+
+    kff_diag = self.kernel.compute_kff_diag(theta)
+    diag1 = (LKinvKuf**2).sum(dim=-2)
+    LKinvLs, _ = torch.triangular_solve(u_tril, Lkuu)
+    vec2 = torch.einsum('...ij,...ik->...jk', LKinvLs, LKinvKuf)
+    diag2 = (vec2**2).sum(dim=-2)
+
+    mu = torch.einsum('...ij,...ik->...jk', LKinvKuf, LKinvu).squeeze(-1)
+    var = kff_diag - diag1 + diag2
+
+    cache = dict(Lkuu=Lkuu)
+
+    return mu, var, cache
+
+  def forward(self, x):
+    '''
     Arguments:
       x: B x in_size
 
@@ -184,42 +224,25 @@ class ContinualSVGP(nn.Module):
       pred_var: n_hypers x out_size x B
     '''
     u_tril = vec2tril(self.u_tril_vec, self.M)
-    hyper_samples = self.kernel.sample_hypers(self.n_hypers)
+    theta = self.kernel.sample_hypers(self.n_hypers)
 
-    kfu = self.kernel.compute_kfu(x, self.z, hyper_samples)
-    kuf = kfu.permute(0, 1, 3, 2)
-    kuu = self.kernel.compute_kuu(self.z, hyper_samples)
-    Lkuu = torch.cholesky(kuu + self.jitter, upper=False)
-    LKinvu, _ = torch.triangular_solve(self.u_mean, Lkuu, upper=False)
-    LKinvKuf, _ = torch.triangular_solve(kuf, Lkuu, upper=False)
-
-    kff_diag = self.kernel.compute_kff_diag(hyper_samples)
-    diag1 = (LKinvKuf**2).sum(dim=-2)
-    LKinvLs, _ = torch.triangular_solve(u_tril, Lkuu)
-    vec2 = torch.einsum('...ij,...ik->...jk', LKinvLs, LKinvKuf)
-    diag2 = (vec2**2).sum(dim=-2)
-
-    pred_mu = torch.einsum('...ij,...ik->...jk', LKinvKuf, LKinvu).squeeze(-1)  # k x out_size x B
-    pred_var = kff_diag - diag1 + diag2
-
-    if info:
-      info = dict(u_tril=u_tril, Lkuu=Lkuu)
-      return pred_mu, pred_var, info
-
-    return pred_mu, pred_var
+    pred_mu, pred_var, cache = self.linear_gauss_conditional(x, self.z, self.u_mean, u_tril, theta)
+    cache = {'u_tril': u_tril, **cache}
+    
+    return pred_mu, pred_var, cache
 
   def loss(self, x, y):
     ## Expected Negative Log Likelihood
-    pred_mu, pred_var, info = self(x, info=True)
+    pred_mu, pred_var, cache = self(x)
     nll = self.likelihood.loss(pred_mu, pred_var, y)
 
     ## TODO(sanyam): Add continual variational/prior distribution
     var_mean = self.u_mean.squeeze(-1).unsqueeze(0).expand(self.n_hypers, -1, -1)
-    var_tril = info.get('u_tril').unsqueeze(0).expand(self.n_hypers, -1, -1, -1)
+    var_tril = cache.get('u_tril').unsqueeze(0).expand(self.n_hypers, -1, -1, -1)
     var_dist = dist.MultivariateNormal(var_mean, scale_tril=var_tril)
 
     prior_mean = torch.zeros_like(self.u_mean).squeeze(-1).unsqueeze(0).expand(self.n_hypers, -1, -1)
-    prior_tril = info.get('Lkuu')
+    prior_tril = cache.get('Lkuu')
     prior_dist = dist.MultivariateNormal(prior_mean, scale_tril=prior_tril)
 
     kl_u = kl_divergence(var_dist, prior_dist).sum(dim=-1).mean(dim=0)
@@ -229,7 +252,7 @@ class ContinualSVGP(nn.Module):
     return nll + kl_u + kl_hypers
 
   def predict(self, x):
-    pred_mu, pred_var = self(x)
+    pred_mu, pred_var, _ = self(x)
     return self.likelihood.predict(pred_mu, pred_var)
 
 
@@ -294,14 +317,14 @@ def main():
   n_classes = y_train.unique().size(0)
 
   # Train GP on only classes 0,1
-  c01_idx = torch.masked_select(torch.arange(y_train.size(0)), (y_train == 0) | (y_train == 1))
+  c01_idx = torch.masked_select(torch.arange(y_train.size(0)).to(device), (y_train == 0) | (y_train == 1))
   c01_gp_state_dict = train_gp(x_train[c01_idx], y_train[c01_idx], n_classes)
   test_gp(c01_gp_state_dict, x_train[c01_idx], y_train[c01_idx], *test_data, n_classes)
 
   z1 = c01_gp_state_dict.get('z')
 
   # Train GP on only classes 2,3
-  c23_idx = torch.masked_select(torch.arange(y_train.size(0)), (y_train == 2) | (y_train == 3))
+  c23_idx = torch.masked_select(torch.arange(y_train.size(0)).to(device), (y_train == 2) | (y_train == 3))
   c23_gp_state_dict = train_gp(x_train[c23_idx], y_train[c23_idx], n_classes)
   test_gp(c23_gp_state_dict, x_train[c23_idx], y_train[c23_idx], *test_data, n_classes)
 
