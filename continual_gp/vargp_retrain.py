a74534edad74681d6df8cb4e6ebe21ddf03aef69
Sanyam Kapoor <sanyam@uber.com>
2020-04-29 15:45:51 -0700

    [wip] retrainable old inducing points

---
+++ continual_gp/vargp_retrain.py
@@ -0,0 +1,251 @@
+import torch
+import torch.nn as nn
+import torch.distributions as dist
+from torch.distributions.kl import kl_divergence
+
+from .gp_utils import vec2tril, cholesky, rev_cholesky, gp_cond, linear_joint, linear_marginal_diag
+from .kernels import RBFKernel
+from .likelihoods import MulticlassSoftmax
+
+
+class VARGPRetrain(nn.Module):
+  def __init__(self, z_init, kernel, likelihood, n_var_samples=1, prev_params=None):
+    super().__init__()
+
+    self.prev_params = prev_params
+    self.retrain_params = prev_params
+    if prev_params:
+      self.retrain_params = nn.ModuleList([
+        nn.ParameterDict(dict(
+          z=nn.Parameter(p['z']),
+          u_mean=nn.Parameter(p['u_mean']),
+          u_tril_vec=nn.Parameter(p['u_tril_vec']),
+        ))
+        for p in prev_params
+      ])
+
+    self.M = z_init.size(-2)
+
+    self.kernel = kernel
+    self.n_v = n_var_samples
+    self.likelihood = likelihood
+
+    self.z = nn.Parameter(z_init.detach())
+
+    out_size = self.z.size(0)
+    self.u_mean = nn.Parameter(torch.Tensor(out_size, self.M, 1).normal_(0., .5))
+    self.u_tril_vec = nn.Parameter(torch.ones(out_size, (self.M * (self.M + 1)) // 2))
+
+  def compute_q(self, theta, prev_params, cache=None):
+    '''
+    Compute variational auto-regressive distributions.
+
+    Arguments:
+      theta: n_hypers x (D + 1)
+
+    Returns
+      mu_lt: n_hypers x out_size x (\sum M_t - M_T) x 1
+      S_lt: n_hypers x out_size x (\sum M_t - M_T) x (\sum M_t - M_T)
+      mu_leq_t: n_hypers x out_size x (\sum M_t) x 1
+      S_leq_t: n_hypers x out_size x (\sum M_t) x (\sum M_t)
+      z_leq_t: out_size x (\sum M_t) x D
+    '''
+    n_hypers = theta.size(0)
+
+    ## Compute q(u_{<t} | \theta)
+    z_lt = prev_params[0]['z']
+    mu_lt = prev_params[0]['u_mean']
+    S_lt = rev_cholesky(vec2tril(prev_params[0]['u_tril_vec']))
+
+    if mu_lt.dim() == 3:
+      mu_lt = mu_lt.unsqueeze(0).expand(n_hypers, -1, -1, -1)
+    if S_lt.dim() == 3:
+      S_lt = S_lt.unsqueeze(0).expand(n_hypers, -1, -1, -1)
+
+    for params in prev_params[1:]:
+      Kzx = self.kernel.compute(theta, z_lt, params['z'])
+      Kzz = self.kernel.compute(theta, z_lt)
+
+      V = rev_cholesky(vec2tril(params['u_tril_vec'])).unsqueeze(0).expand(n_hypers, -1, -1, -1)
+      b = params['u_mean'].unsqueeze(0).expand(n_hypers, -1, -1, -1)
+
+      mu_lt, S_lt = linear_joint(mu_lt, S_lt, Kzx, Kzz, V, b)
+      z_lt = torch.cat([z_lt, params['z']], dim=-2)
+
+    ## Compute q(u_{\leq t} | \theta)
+    Kzx = self.kernel.compute(theta, z_lt, self.z)
+    Kzz = self.kernel.compute(theta, z_lt)
+
+    V = rev_cholesky(vec2tril(self.u_tril_vec)).unsqueeze(0).expand(n_hypers, -1, -1, -1)
+    b = self.u_mean.unsqueeze(0).expand(n_hypers, -1, -1, -1)
+
+    cache_leq_t = dict()
+    mu_leq_t, S_leq_t = linear_joint(mu_lt, S_lt, Kzx, Kzz, V, b, cache=cache_leq_t)
+    z_leq_t = torch.cat([z_lt, self.z], dim=-2)
+
+    if isinstance(cache, dict):
+      cache['Lz_lt'] = cache_leq_t['Lz']
+      cache['Lz_lt_Kz_lt_z_t'] = cache_leq_t['Lz_Kzx']
+
+    return mu_lt, S_lt, \
+           mu_leq_t, S_leq_t, \
+           z_leq_t
+
+  def compute_pf_diag(self, theta, x, mu_leq_t, S_leq_t, z_leq_t, cache=None):
+    '''
+    Compute p(f) = \int p(f|u_{\leq t})q(u_{\leq t}).
+    Only diagonal of covariance for p(f) is used.
+
+    Arguments:
+      theta: n_hypers x (D + 1)
+      x: B x D
+      mu_leq_t: [n_hypers] x out_size x (\sum M_t) x 1
+      S_leq_t: [n_hypers] x out_size x (\sum M_t) x (\sum M_t)
+      z_leq_t: out_size x (\sum M_t) x D
+
+    Returns:
+      f_mean: n_hypers x out_size x B
+      f_var: n_hypers x out_size x B
+    '''
+    xf = x.unsqueeze(0).expand(z_leq_t.size(0), -1, -1)
+
+    Kzz = self.kernel.compute(theta, z_leq_t)
+    Kzx = self.kernel.compute(theta, z_leq_t, xf)
+    Kxx_diag = self.kernel.compute_diag(theta)
+
+    f_mean, f_var = linear_marginal_diag(mu_leq_t, S_leq_t, Kzz, Kzx, Kxx_diag, cache=cache)
+    return f_mean, f_var
+
+  def forward(self, x, loss_cache=False):
+    '''
+    Arguments:
+      x: B x in_size
+
+    Returns:
+      Output distributions for n_hypers samples of hyperparameters.
+      The output contains only diagonal of the full covariance.
+
+      pred_mu: n_hypers x out_size x B
+      pred_var: n_hypers x out_size x B
+    '''
+    theta = self.kernel.sample_hypers(self.n_v)
+
+    if self.prev_params:
+      cache_q, cache_q_tilde = dict(), dict()
+
+      mu_lt, S_lt, *_ = self.compute_q(theta, self.retrain_params, cache=cache_q)
+
+      mu_lt_tilde, S_lt_tilde, mu_leq_t_tilde, S_leq_t_tilde, z_leq_t_tilde = self.compute_q(theta, self.prev_params, cache=cache_q_tilde)
+
+      pred_mu, pred_var = self.compute_pf_diag(theta, x, mu_leq_t_tilde, S_leq_t_tilde, z_leq_t_tilde)
+
+      if isinstance(loss_cache, dict):
+        q_lt = dist.MultivariateNormal(mu_lt.squeeze(-1), covariance_matrix=S_lt)
+        u_lt = q_lt.rsample(torch.Size([self.n_v])).unsqueeze(-1)
+        # u_lt = mu_lt.unsqueeze(0)
+
+        if u_lt.dim() == 4:
+          u_lt = u_lt.unsqueeze(1)
+
+        ## Compute q(u_t | u_{<t}, \theta)
+        Lz = cache_q.pop('Lz_lt').unsqueeze(0)
+        Lz_Kzx = cache_q.pop('Lz_lt_Kz_lt_z_t').unsqueeze(0).expand(self.n_v, *([-1] * (Lz.dim() - 1)))
+        Kzz = self.kernel.compute(theta, self.z).unsqueeze(0)
+        var_mu_t, _ = gp_cond(u_lt, None, None, Kzz, Lz=Lz, Lz_Kzx=Lz_Kzx)
+        var_mu_t = var_mu_t + self.u_mean.unsqueeze(0).unsqueeze(0)
+        var_L_cov_t = vec2tril(self.u_tril_vec, self.M).unsqueeze(0).unsqueeze(0)
+
+        ## Compute p(u_t | \tilde{u}_{<t}, \theta)
+        Lz = cache_q_tilde.pop('Lz_lt')#.unsqueeze(0)
+        Lz_Kzx = cache_q_tilde.pop('Lz_lt_Kz_lt_z_t')#.unsqueeze(0).expand(self.n_v, *([-1] * (Lz.dim() - 1)))
+        prior_mu_t, prior_cov_t = gp_cond(mu_lt_tilde, None, None, Kzz, Lz=Lz, Lz_Kzx=Lz_Kzx)
+        prior_mu_t = prior_mu_t.unsqueeze(0)
+
+        loss_cache.update(dict(var_mu_t=var_mu_t.squeeze(-1), var_L_cov_t=var_L_cov_t,
+                               var_m_lt=mu_lt.squeeze(-1), var_S_lt=S_lt, var_m_lt_tilde=mu_lt_tilde.squeeze(-1), var_S_lt_tilde=S_lt_tilde,
+                               prior_mu_t=prior_mu_t.squeeze(-1), prior_L_cov_t=cholesky(prior_cov_t)))
+    else:
+      cache_pf = dict()
+
+      mu_leq_t = self.u_mean
+      L_cov_leq_t = vec2tril(self.u_tril_vec, self.M)
+
+      pred_mu, pred_var = self.compute_pf_diag(theta, x, mu_leq_t, rev_cholesky(L_cov_leq_t), self.z, cache=cache_pf)
+
+      if isinstance(loss_cache, dict):
+        # Compute q(u_1)
+        mu_t = mu_leq_t.squeeze(-1).unsqueeze(0).unsqueeze(0)
+        L_cov_t = L_cov_leq_t.unsqueeze(0).unsqueeze(0)
+
+        # Compute p(u_1)
+        prior_mu_t = torch.zeros_like(mu_t)
+        prior_L_cov_t = cache_pf.pop('Lz').unsqueeze(0)
+
+        loss_cache.update(dict(var_mu_t=mu_t, var_L_cov_t=L_cov_t, prior_mu_t=prior_mu_t, prior_L_cov_t=prior_L_cov_t))
+
+    return pred_mu, pred_var
+
+  def loss(self, x, y):
+    loss_cache = dict()
+    pred_mu, pred_var = self(x, loss_cache=loss_cache)
+    nll = self.likelihood.loss(pred_mu, pred_var, y)
+
+    kl_u_lt = torch.tensor(0.0, device=x.device)
+    if 'var_m_lt' in loss_cache:
+      q_lt = dist.MultivariateNormal(
+        loss_cache.pop('var_m_lt'),
+        covariance_matrix=loss_cache.pop('var_S_lt')
+      )
+      q_lt_tilde = dist.MultivariateNormal(
+        loss_cache.pop('var_m_lt_tilde'),
+        covariance_matrix=loss_cache.pop('var_S_lt_tilde')
+      )
+      kl_u_lt = kl_divergence(q_lt, q_lt_tilde).sum(dim=-1).mean(dim=0)
+
+    var_dist = dist.MultivariateNormal(
+      loss_cache.pop('var_mu_t'),
+      scale_tril=loss_cache.pop('var_L_cov_t'))
+
+    prior_dist = dist.MultivariateNormal(
+      loss_cache.pop('prior_mu_t'),
+      scale_tril=loss_cache.pop('prior_L_cov_t'))
+
+    kl_u = kl_divergence(var_dist, prior_dist).sum(dim=-1).mean(dim=0).mean(dim=0)
+
+    kl_hypers = self.kernel.kl_hypers()
+
+    return kl_hypers, kl_u, kl_u_lt, nll
+
+  def predict(self, x):
+    pred_mu, pred_var = self(x)
+    return self.likelihood.predict(pred_mu, pred_var)
+
+  @staticmethod
+  def create_clf(dataset, M=20, n_f=10, n_var_samples=3, prev_params=None):
+    N = len(dataset)
+    out_size = torch.unique(dataset.targets).size(0)
+
+    ## init inducing points at random data points.
+    z = torch.stack([
+      dataset[torch.randperm(N)[:M]][0]
+      for _ in range(out_size)])
+
+    prior_log_mean, prior_log_logvar = None, None
+
+    ## TODO(sanyam): Need to handle all the new param structure for T>2.
+    if prev_params:
+      prior_log_mean = prev_params[-1].get('kernel.log_mean')
+      prior_log_logvar = prev_params[-1].get('kernel.log_logvar')
+
+      def process(p):
+        for k in list(p.keys()):
+          if k.startswith('kernel'):
+            p.pop(k)
+        return p
+
+      prev_params = [process(p) for p in prev_params]
+
+    kernel = RBFKernel(z.size(-1), prior_log_mean=prior_log_mean, prior_log_logvar=prior_log_logvar)
+    likelihood = MulticlassSoftmax(n_f=n_f)
+    gp = VARGPRetrain(z, kernel, likelihood, n_var_samples=n_var_samples, prev_params=prev_params)
+    return gp
---
+++ experiments/vargp_retrain.py
@@ -0,0 +1,111 @@
+import os
+from tqdm.auto import tqdm
+import wandb
+import torch
+from torch.utils.data import DataLoader, ConcatDataset
+from torch.utils.tensorboard import SummaryWriter
+import torch_optimizer
+
+from continual_gp.datasets import ToyDataset
+from continual_gp.train_utils import set_seeds, EarlyStopper, compute_accuracy
+from continual_gp.vargp_retrain import VARGPRetrain
+
+
+def train(task_id, train_set, val_set, test_set, ep_var_mean=True, map_est_hypers=False,
+          epochs=1, M=20, n_f=10, n_var_samples=3, batch_size=512, lr=1e-2, beta=1.0,
+          eval_interval=10, patience=20, prev_params=None, logger=None, device=None):
+  gp = VARGPRetrain.create_clf(train_set, M=M, n_f=n_f, n_var_samples=n_var_samples,
+                               prev_params=prev_params).to(device)
+
+  stopper = EarlyStopper(patience=patience)
+
+  # optim = torch.optim.Adam(gp.parameters(), lr=lr)
+  optim = torch_optimizer.Yogi(gp.parameters(), lr=lr)
+
+  N = len(train_set)
+  loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
+
+  for e in tqdm(range(epochs)):
+    for x, y in tqdm(loader, leave=False):
+      optim.zero_grad()
+
+      kl_hypers, kl_u, kl_u_lt, lik = gp.loss(x.to(device), y.to(device))
+
+      loss = beta * kl_hypers + kl_u + kl_u_lt + (N / x.size(0)) * lik
+      loss.backward()
+
+      optim.step()
+
+    if (e + 1) % eval_interval == 0:
+      train_acc = compute_accuracy(train_set, gp, device=device)
+      val_acc = compute_accuracy(val_set, gp, device=device)
+      test_acc = compute_accuracy(test_set, gp, device=device)
+
+      loss_summary = {
+        f'task{task_id}/loss/kl_hypers': kl_hypers.detach().item(),
+        f'task{task_id}/loss/kl_u': kl_u.detach().item(),
+        f'task{task_id}/loss/kl_u_lt': kl_u_lt.detach().item(),
+        f'task{task_id}/loss/lik': lik.detach().item()
+      }
+
+      acc_summary = {
+        f'task{task_id}/train/acc': train_acc,
+        f'task{task_id}/val/acc': val_acc,
+        f'task{task_id}/test/acc': test_acc,
+      }
+
+      if logger is not None:
+        for k, v in (dict(**loss_summary, **acc_summary)).items():
+          logger.add_scalar(k, v, global_step=e + 1)
+
+      stopper(val_acc, dict(state_dict=gp.state_dict(), acc_summary=acc_summary, step=e + 1))
+      if stopper.is_done():
+        break
+
+  info = stopper.info()
+  if logger is not None:
+    for k, v in info.get('acc_summary').items():
+      logger.add_scalar(f'{k}_best', v, global_step=info.get('step'))
+
+    with open(f'{logger.log_dir}/ckpt{task_id}.pt', 'wb') as f:
+      torch.save(info.get('state_dict'), f)
+    wandb.save(f'{logger.log_dir}/ckpt{task_id}.pt')
+
+  return info.get('state_dict')
+
+
+def toy(data_dir='/tmp', epochs=5000, M=20, lr=1e-2,
+        batch_size=512, beta=1.0, ep_var_mean=True, map_est_hypers=False,
+        seed=None):
+  set_seeds(seed)
+
+  wandb.init(tensorboard=True)
+
+  device = 'cuda' if torch.cuda.is_available() else 'cpu'
+  logger = SummaryWriter(log_dir=wandb.run.dir)
+
+  toy_train = ToyDataset()
+  toy_val = ToyDataset(X=toy_train.data.clone(), Y=toy_train.targets.clone())
+  toy_test = ToyDataset(X=toy_train.data.clone(), Y=toy_train.targets.clone())
+
+  prev_params = []
+  for t in range(2):
+    toy_train.filter_by_class([2 * t, 2 * t + 1])
+    toy_val.filter_by_class(range(2 * t + 2))
+    toy_test.filter_by_class(range(2 * t + 2))
+
+    state_dict = train(t, toy_train, toy_val, toy_test,
+                       epochs=epochs, M=M, lr=lr, beta=beta, batch_size=batch_size,
+                       ep_var_mean=bool(ep_var_mean), map_est_hypers=bool(map_est_hypers),
+                       prev_params=prev_params, logger=logger, device=device, patience=-1)
+
+    prev_params.append(state_dict)
+
+  logger.close()
+
+
+if __name__ == "__main__":
+  os.environ['WANDB_MODE'] = 'run' if os.environ.get('IS_UBUILD') else 'dryrun'
+
+  import fire
+  fire.Fire(dict(toy=toy))
