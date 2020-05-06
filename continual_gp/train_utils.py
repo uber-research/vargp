62179f302a9167c06ebc204ffeef052879b543c1
Sanyam Kapoor <sanyam@uber.com>
2020-05-05 17:46:34 -0700

    [wip] add bwt

--- continual_gp/train_utils.py
+++ continual_gp/train_utils.py
@@ -2,6 +2,7 @@ import random
 import numpy as np
 import torch
 import torch_optimizer
+from torch.distributions import Categorical
 from torch.utils.data import DataLoader
 from tqdm.auto import tqdm
 import wandb
@@ -32,6 +33,36 @@ def compute_accuracy(dataset, gp, batch_size=512, device=None):
     acc = count / len(dataset)
 
   return acc
+
+
+def compute_acc_ent(dataset, gp, batch_size=512, device=None):
+  loader = DataLoader(dataset, batch_size=batch_size)
+
+  with torch.no_grad():
+    total_corr = 0
+    total_ent = 0.
+
+    for x, y in tqdm(loader, leave=False):
+      preds = gp.predict(x.to(device))
+
+      assert not torch.isnan(preds).any(), 'Found NaNs'
+
+      total_corr += (preds.argmax(dim=-1) == y.to(device)).sum().item()
+      total_ent += Categorical(probs=preds).entropy().sum().item()
+
+  mean_acc = total_corr / len(dataset)
+  mean_ent = total_ent / len(dataset)
+
+  return mean_acc, mean_ent
+
+
+def compute_bwt(acc_mat):
+    assert acc_mat.ndim == 2
+    assert acc_mat.shape[0] == acc_mat.shape[1]
+
+    T = acc_mat.shape[0]
+
+    return (acc_mat[-1][:-1] - acc_mat.diagonal()[:-1]).mean()
 
 
 # Reference: https://github.com/Bjarten/early-stopping-pytorch/blob/master/pytorchtools.py
